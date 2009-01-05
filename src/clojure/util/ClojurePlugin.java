88ebd9685557dffaad0629af2c221afa81a9c6b6
casey.marshall <none@none>
2009-01-05 04:04:06 +0000

    Did a bit of class name refactoring. Clojure nature now adds the clojure bundle classpath to the JDT classpath, launcher is able to successfully start a REPL in a separate VM.
    --HG--
    rename : src/clojure/util/Activator.java => src/clojure/util/ClojurePlugin.java
    extra : convert_revision : svn%3A951d75ee-2d4e-0410-a0c2-87c8fdd8ec4c/clojure/trunk%4072

--- src/clojure/util/ClojurePlugin.java
+++ src/clojure/util/ClojurePlugin.java
@@ -1,5 +1,9 @@
 package clojure.util;
 
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPluginDescriptor;
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
@@ -7,16 +11,16 @@ import org.osgi.framework.BundleContext;
 import clojure.lang.RT;
 
 
-public class Activator extends Plugin {
+public class ClojurePlugin extends Plugin {
 
-	static private Activator plugin;
+	static private ClojurePlugin plugin;
 	
-	public Activator() {
+	public ClojurePlugin() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Activator(IPluginDescriptor descriptor) {
+	public ClojurePlugin(IPluginDescriptor descriptor) {
 		super(descriptor);
 		// TODO Auto-generated constructor stub
 	}
@@ -36,8 +40,21 @@ public class Activator extends Plugin {
 		plugin = null;
 	}
 
-	static public Activator getDefault() {
+	static public ClojurePlugin getDefault() {
 		return plugin;
 	}
 	
+    public String binPath() {
+        URL bpath = getBundle().getEntry("/");
+        URL rpath;
+        try {
+            rpath = FileLocator.resolve(bpath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        String base = rpath.getPath() + "bin/";
+        return base;
+    }
+    
 }
